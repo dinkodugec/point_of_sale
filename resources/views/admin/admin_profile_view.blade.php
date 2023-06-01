adf829cb12281185796f1c49d62290b6d0435086
Dinko <dinko.dugec@gmail.com>
2023-06-01 23:23:57 +0200

    prepare for image upload

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -27,11 +27,11 @@
 <div class="col-lg-4 col-xl-4">
 <div class="card text-center">
 <div class="card-body">
-<img src="assets/images/users/user-1.jpg" class="rounded-circle avatar-lg img-thumbnail"
+<img src="{{ (!empty($adminData->photo)) ? url('upload/admin_image/'.$adminData->photo) : url('upload/no_image.jpg') }}" class="rounded-circle avatar-lg img-thumbnail"
 alt="profile-image">
 
-<h4 class="mb-0">Geneva McKnight</h4>
-<p class="text-muted">@webdesigner</p>
+<h4 class="mb-0">{{ $adminData->name }}</h4>
+<p class="text-muted">{{ $adminData->email }}</p>
 
 <button type="button" class="btn btn-success btn-xs waves-effect mb-2 waves-light">Follow</button>
 <button type="button" class="btn btn-danger btn-xs waves-effect mb-2 waves-light">Message</button>
@@ -39,13 +39,12 @@ alt="profile-image">
 <div class="text-start mt-3">
 
 
-    <p class="text-muted mb-2 font-13"><strong>Full Name :</strong> <span class="ms-2">Geneva D. McKnight</span></p>
+    <p class="text-muted mb-2 font-13"><strong>{{ $adminData->name }}</strong></p>
 
-    <p class="text-muted mb-2 font-13"><strong>Mobile :</strong><span class="ms-2">(123) 123 1234</span></p>
+    <p class="text-muted mb-2 font-13"><strong>{{ $adminData->phone }}</strong></p>
 
-    <p class="text-muted mb-2 font-13"><strong>Email :</strong> <span class="ms-2">user@email.domain</span></p>
+    <p class="text-muted mb-2 font-13"><strong>{{ $adminData->email }}</strong></p>
 
-    <p class="text-muted mb-1 font-13"><strong>Location :</strong> <span class="ms-2">USA</span></p>
 </div>                                    
 
 <ul class="social-list list-inline mt-3 mb-0">
@@ -79,30 +78,57 @@ alt="profile-image">
 
 <!-- end timeline content-->
 
-<div class="tab-pane" id="settings">
-<form>
-<h5 class="mb-4 text-uppercase"><i class="mdi mdi-account-circle me-1"></i> Personal Info</h5>
-<div class="row">
-<div class="col-md-6">
-    <div class="mb-3">
-        <label for="firstname" class="form-label">First Name</label>
-        <input type="text" class="form-control" id="firstname" placeholder="Enter first name">
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="mb-3">
-        <label for="lastname" class="form-label">Last Name</label>
-        <input type="text" class="form-control" id="lastname" placeholder="Enter last name">
-    </div>
-</div> <!-- end col -->
-</div> <!-- end row -->
+ <div class="tab-pane" id="settings">
+        <form>
+            <h5 class="mb-4 text-uppercase"><i class="mdi mdi-account-circle me-1"></i> Personal Info</h5>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">First Name</label>
+                        <input type="text" class="form-control" id="name" value="{{ $adminData->name }}">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" class="form-control" id="email" value="{{ $adminData->email}}" >
+                    </div>
+                </div> 
+
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="phone" class="form-label">Phone</label>
+                        <input type="text" name="phone" class="form-control" id="phone" value="{{ $adminData->phone }}">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="lastname" class="form-label">Phone</label>
+                        <input type="text" name="phone" class="form-control" id="lastname"  value="{{ $adminData->phone }}" >
+                    </div>
+                </div> <!-- end col -->
+               <div class="col-md-12">
+                    <div class="mb-3">
+                     <label for="example-fileinput" class="form-label">Admin Profile Image</label>
+                     <input type="file" id="example-fileinput" class="form-control">
+                   </div>
+              </div> <!-- end col -->
+             <div class="col-md-12">
+               <div class="mb-3">
+                 <label for="example-fileinput" class="form-label"> </label>
+                <img src="{{ (!empty($adminData->photo)) ? url('upload/admin_image/'.$adminData->photo) : url('upload/no_image.jpg') }}" class="rounded-circle avatar-lg img-thumbnail"
+                         alt="profile-image">
+              </div>
+            </div> <!-- end col -->
+
+            </div> <!-- end row -->
 
 
 
-<div class="text-end">
-<button type="submit" class="btn btn-success waves-effect waves-light mt-2"><i class="mdi mdi-content-save"></i> Save</button>
-</div>
-</form>
+            <div class="text-end">
+              <button type="submit" class="btn btn-success waves-effect waves-light mt-2"><i class="mdi mdi-content-save"></i> Save</button>
+            </div>
+        </form>
 </div>
 <!-- end settings content-->
 
