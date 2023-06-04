2a13a07a0d7026312ff2ae3d25e161975b0c2774
Dinko <dinko.dugec@gmail.com>
2023-06-05 00:18:03 +0200

    toaster to login and logout page

--- resources/views/admin/admin_logout.blade.php
+++ resources/views/admin/admin_logout.blade.php
@@ -21,6 +21,15 @@
        <!-- Head js -->
        <script src="{{ asset('backend/assets/js/head.js') }}"></script>
 
+         <!-- toastr -->
+         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+         <!-- toastr -->
+ 
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ 
+         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+ 
+
     </head>
 
     <body class="authentication-bg authentication-bg-pattern">
@@ -93,6 +102,28 @@
 
         <!-- App js -->
         <script src="{{ asset('backend/assets/js/app.min.js') }}"></script>
+
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
+        <script>
+            @if(Session::has('message'))
+            var type = "{{ Session::get('alert-type','info') }}"
+            switch(type){
+               case 'info':
+               toastr.info(" {{ Session::get('message') }} ");
+               break;
+               case 'success':
+               toastr.success(" {{ Session::get('message') }} ");
+               break;
+               case 'warning':
+               toastr.warning(" {{ Session::get('message') }} ");
+               break;
+               case 'error':
+               toastr.error(" {{ Session::get('message') }} ");
+               break; 
+            }
+            @endif 
+           </script>
         
     </body>
 </html>
