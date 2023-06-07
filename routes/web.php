263add395c15fb57498d8b080dbbd4270249248e
Dinko <dinko.dugec@gmail.com>
2023-06-07 21:30:16 +0200

    middleware auth

--- routes/web.php
+++ routes/web.php
@@ -35,12 +35,10 @@ Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('adm
 
 Route::get('/logout', [AdminController::class, 'AdminLogoutPage'])->name('admin.logout.page');
 
+Route::middleware(['auth'])->group(function() {
 
-Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
-
-
-Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
-
-Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
-
-Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
+    Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+});  //auth middleware
