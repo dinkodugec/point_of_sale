d298cbfdb92b224cfc26ad037d501fd8ea25e182
Dinko <dinko.dugec@gmail.com>
2023-08-21 00:16:33 +0200

    add product, with eloquent relationship

--- routes/web.php
+++ routes/web.php
@@ -129,6 +129,7 @@ Route::controller(SupplierController::class)->group(function(){
    Route::controller(ProductController::class)->group(function(){
    Route::get('/all/product','AllProduct')->name('all.product');
    Route::get('/add/product','AddProduct')->name('add.product');
+   Route::post('/store/product','StoreProduct')->name('product.store');
   });
   
   
