65a9fda55b14924acc445044d6b6c7aa084e929c
Dinko <dinko.dugec@gmail.com>
2023-09-21 00:11:59 +0200

    edit expense

--- routes/web.php
+++ routes/web.php
@@ -148,6 +148,8 @@ Route::controller(SupplierController::class)->group(function(){
     Route::get('/add/expense','AddExpense')->name('add.expense');
     Route::post('/store/expense','StoreExpense')->name('expense.store');
     Route::get('/today/expense','TodayExpense')->name('today.expense');
+    Route::get('/edit/expense/{id}','EditExpense')->name('edit.expense');
+    Route::post('/update/expense','UpdateExpense')->name('expense.update');
   
   });
   
