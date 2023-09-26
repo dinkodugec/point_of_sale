37b0ffd8ed4b1b7fc67cacbeff51ed70a3fa7043
Dinko <dinko.dugec@gmail.com>
2023-09-26 23:36:18 +0200

    pos page finished design

--- app/Http/Controllers/Backend/PosController.php
+++ app/Http/Controllers/Backend/PosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -11,7 +12,8 @@ class PosController extends Controller
     public function Pos()
     {
         $product = Product::latest()->get();
-        return view('backend.pos.pos_page',compact('product'));
+        $customer = Customer::latest()->get();
+        return view('backend.pos.pos_page',compact('product','customer'));
 
     } 
 
