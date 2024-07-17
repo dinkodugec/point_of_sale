8a17a849110e192d2879d5b291bff52277714254
Dinko <dinko.dugec@gmail.com>
2024-07-17 23:20:22 +0200

    Add Roles in Permission Part 2

--- app/Http/Controllers/Backend/RoleController.php
+++ app/Http/Controllers/Backend/RoleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
@@ -174,8 +175,9 @@ public function AddRolesPermission()
 
     $roles = Role::all();
     $permissions = Permission::all();
-    return view('backend.pages.roles.add_roles_permission',compact('roles','permissions'));
 
+    $permission_groups = User::getpermissionGroups();
+    return view('backend.pages.roles.add_roles_permission',compact('roles','permissions','permission_groups'));
 }// End Method 
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -45,4 +46,21 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function getpermissionGroups()
+    {
+        $permission_groups = DB::table('permissions')->select('group_name')->groupBy('group_name')->get();
+        return $permission_groups;
+    } // End Method 
+
+
+    public static function getpermissionByGroupName($group_name)
+    {
+        $permissions = DB::table('permissions')
+                        ->select('name','id')
+                        ->where('group_name',$group_name)
+                        ->get();
+          return $permissions;
+
+    }// End Method 
 }
